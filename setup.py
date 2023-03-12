a4716c8301fce146b60c65483804c7b60498b1ea
Julius-W <81867196+Julius-W@users.noreply.github.com>
2023-03-12 01:51:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
 from setuptools import setup, find_packages
+import os
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), 'r', encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.2.5'
 
 setup(
-    name='InstaBot',
-    version='0.1.0',
-    description='A simple library for managing an Instagram bot.',
+    name='InstaWebBot',
+    version=VERSION,
+    author='Julius-W',
+    description='A simple library for managing an Instagram bot using selenium.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     url='https://github.com/Julius-W/InstaBot',
-    packages=find_packages(),
+    license='GPL-2.0',
+    packages=['InstaWebBot'],
     python_requires='>=3.6',
     install_requires=['selenium'],
+    keywords=['python', 'Instagram', 'bot', 'automation'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    ]
 )
