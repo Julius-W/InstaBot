1e67db2f4ef27c1b42a4e559aea4b835ed9b0b3e
Julius-W <81867196+Julius-W@users.noreply.github.com>
2023-03-12 01:51:54 +0100

    Create __innit__.py

---
+++ InstaWebBot/__innit__.py
@@ -0,0 +1,9 @@
+"""InstaBot is made to simplify a few tasks for bots on Instagram with selenium."""
+
+__author__ = "Julius-W"
+__version__ = "0.2.5"
+__license__ = "GPL-2.0"
+
+__all__ = ['Instabot']
+
+from .Instabot import InstaBot
